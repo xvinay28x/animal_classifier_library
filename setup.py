fb395ca0c7a580e7bd55bd6a5f21de44c1d0d04e
Vinay Garg <xvinay28x@gmail.com>
2021-05-20 21:48:51 +0530

    adding url of github

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     long_description_content_type='text/markdown',
     author="Vinay Garg",
     author_email="xvinay28x@gmail.com",
+    url="https://github.com/xvinay28x/cat_dog_classifier_library",
     install_requires=["tensorflow"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
